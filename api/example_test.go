97db0c651c3026f9f61e1057ff4d4818af108d60
Maxime Corbin <maxime.corbin@skillz.io>
2021-01-16 22:25:07 +0100

    fix(test): removed output so that the example is not ran

--- api/example_test.go
+++ api/example_test.go
@@ -10,12 +10,10 @@ func ExampleAPI_Login() {
 	c := api.New(&http.Client{}, "xxxx")
 	key, err := c.Login("login", "password")
 	fmt.Println(key, err)
-	// Output: auth_XXXX, nil
 }
 
 func ExampleAPI_Upload() {
 	c := api.New(&http.Client{}, "xxxx")
 	err := c.Upload([]string{"../../testhelpers/tars/single.tar"})
 	fmt.Println(err)
-	// Output: nil
 }
