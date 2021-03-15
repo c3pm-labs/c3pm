da2f6ad1c87f9ccf24dcc6d233c895f4a6fa50af
Gabriel Colson <gabrielcolson@hotmail.fr>
2021-03-15 10:25:48 +0100

    fix: remove debug in cmake generation

--- adapter/builtin/exec.go
+++ adapter/builtin/exec.go
@@ -34,11 +34,6 @@ target_include_directories(
 find_library({{ .Name | ToUpper}} {{.Name}} "{{$c3pmGlobalDir}}/cache/{{.Name}}/{{.Version}}/")
 {{end}}
 
-{{range .Dependencies}}
-message(STATUS {{"${"}}{{.Name|ToUpper}}{{"}"}})
-file(GENERATE OUTPUT ./toto.txt CONTENT $<STREQUAL:"{{"${"}}{{.Name|ToUpper}}{{"}"}}","HELLO_WORLD-NOTFOUND">)
-{{- end}}
-
 target_link_libraries(
 	{{.ProjectName}}
 	PUBLIC
