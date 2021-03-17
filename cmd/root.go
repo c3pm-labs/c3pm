715dd5993e6da5a95b2e215c5ca56b3e749275c5
Chloé <chloe.bourbion@gmail.com>
2021-03-17 21:50:33 +0100

    fix: lint

--- cmd/root.go
+++ cmd/root.go
@@ -7,7 +7,7 @@ import "github.com/alecthomas/kong"
 var CLI struct {
 	Version kong.VersionFlag `short:"v" help:"outputs the version number"`
 	Add     AddCmd           `kong:"cmd,help='Add a new dependency'"`
-	Remove	RemoveCmd		 `kong:"cmd,help='Remove a dependency'"`
+	Remove  RemoveCmd        `kong:"cmd,help='Remove a dependency'"`
 	Init    InitCmd          `kong:"cmd,help='Init a c3pm project'"`
 	Logout  LogoutCmd        `kong:"cmd,help='Logout from the api'"`
 	Login   LoginCmd         `kong:"cmd,help='Login to the api'"`
