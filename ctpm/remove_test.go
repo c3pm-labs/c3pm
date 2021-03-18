9357c8db6cf2202c7548336c122595829643997e
Chloé <chloe.bourbion@gmail.com>
2021-03-18 16:37:07 +0100

    add more test on remove command

--- ctpm/remove_test.go
+++ ctpm/remove_test.go
@@ -24,24 +24,26 @@ var _ = Describe("Remove", func() {
 			"super-math":  "1.0.0",
 		}
 		var pc *config.ProjectConfig
+		err := os.MkdirAll(projectFolder, os.ModePerm)
+		Ω(err).ShouldNot(HaveOccurred())
 
 		BeforeEach(func() {
-			var err error
 			m := manifest.New()
 			m.Name = projectName
 			m.Version, err = manifest.VersionFromString("1.0.0")
 			Ω(err).ShouldNot(HaveOccurred())
-
 			m.Type = projectType
 			m.License = projectLicense
 			m.Description = projectDesc
 			m.Dependencies = dependencies
+
 			pc = &config.ProjectConfig{Manifest: m, ProjectRoot: projectFolder}
-			err = os.MkdirAll(projectFolder, os.ModePerm)
-			Ω(err).ShouldNot(HaveOccurred())
+
 			_, err = os.Create(filepath.Join(projectFolder, "c3pm.yml"))
 			Ω(err).ShouldNot(HaveOccurred())
-
+		})
+		AfterEach(func() {
+			os.Remove(filepath.Join(projectFolder, "c3pm.yml"))
 		})
 
 		It("remove one dependency", func() {
@@ -51,6 +53,36 @@ var _ = Describe("Remove", func() {
 			_, ok := pc.Manifest.Dependencies[toRemove[0]]
 			Ω(ok).Should(BeFalse())
 
+		})
+		It("remove several dependencies at the same time", func() {
+			toRemove := []string{"calculator", "sort", "simple-math"}
+			err := ctpm.Remove(pc, ctpm.RemoveOptions{Dependencies: toRemove})
+			Ω(err).ShouldNot(HaveOccurred())
+			for _, dep := range toRemove {
+				_, ok := pc.Manifest.Dependencies[dep]
+				Ω(ok).Should(BeFalse())
+			}
+		})
+		It("remove unexisting dependency", func() {
+			toRemove := []string{"toto"}
+			err := ctpm.Remove(pc, ctpm.RemoveOptions{Dependencies: toRemove})
+			Ω(err).ShouldNot(HaveOccurred())
+			for _, dep := range toRemove {
+				_, ok := pc.Manifest.Dependencies[dep]
+				Ω(ok).Should(BeFalse())
+			}
+		})
+		It("remove dependency from an empty dependency array", func() {
+			pc.Manifest.Dependencies = map[string]string{}
+			toRemove := []string{"toto"}
+			err := ctpm.Remove(pc, ctpm.RemoveOptions{Dependencies: toRemove})
+			Ω(err).Should(HaveOccurred())
+		})
+		It("remove dependency from an nil dependency array", func() {
+			pc.Manifest.Dependencies = nil
+			toRemove := []string{"toto"}
+			err := ctpm.Remove(pc, ctpm.RemoveOptions{Dependencies: toRemove})
+			Ω(err).Should(HaveOccurred())
 		})
 	})
 })
