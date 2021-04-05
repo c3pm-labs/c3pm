a8fc1a69b15a5c3df9775a0b9d28b4eec6a16909
Jules Casteran <jules.casteran@gmail.com>
2021-04-05 16:09:01 -0400

    feat: switch init cmd to cobra

--- cmd/init.go
+++ cmd/init.go
@@ -5,29 +5,44 @@ import (
 	"github.com/c3pm-labs/c3pm/cmd/input"
 	"github.com/c3pm-labs/c3pm/config"
 	"github.com/c3pm-labs/c3pm/ctpm"
+	"github.com/spf13/cobra"
 	"path/filepath"
 )
 
-//InitCmd defines the parameters of the init command.
-type InitCmd struct {
-	NoTemplate bool   `kong:"optional,name='no-template',help='Prevents the creation of CMake files'"`
-	Path       string `kong:"optional,arg,name='path',help='Project path, default to working directory',default='.'"`
+var initCmdFlags = ctpm.InitOptions{}
+
+var initCmd = &cobra.Command{
+	Use: "init [path]",
+	Short: "Init a c3pm project",
+	Long: "Init a c3pm project\n\n" +
+		"Project path defaults to working directory",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) > 1 {
+			return fmt.Errorf("requires one or no arguments")
+		} else if len(args) == 0 {
+			cmd.SetArgs([]string{"."})
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		path := args[0]
+		manifest, err := input.Init()
+		if err != nil {
+			return fmt.Errorf("failed to init project config: %w", err)
+		}
+		projectRoot, err := filepath.Abs(path)
+		if err != nil {
+			return err
+		}
+		pc := &config.ProjectConfig{Manifest: manifest, ProjectRoot: projectRoot}
+		err = ctpm.Init(pc, initCmdFlags)
+		if err != nil {
+			return fmt.Errorf("failed to init project: %w", err)
+		}
+		return nil
+	},
 }
 
-//Run handles the behavior of the init command.
-func (i *InitCmd) Run() error {
-	manifest, err := input.Init()
-	if err != nil {
-		return fmt.Errorf("failed to init project config: %w", err)
-	}
-	projectRoot, err := filepath.Abs(i.Path)
-	if err != nil {
-		return err
-	}
-	pc := &config.ProjectConfig{Manifest: manifest, ProjectRoot: projectRoot}
-	err = ctpm.Init(pc, ctpm.InitOptions{NoTemplate: i.NoTemplate})
-	if err != nil {
-		return fmt.Errorf("failed to init project: %w", err)
-	}
-	return nil
+func init() {
+	initCmd.Flags().BoolVar(&initCmdFlags.NoTemplate, "no-template", ctpm.InitDefaultOptions.NoTemplate, "Prevents the creation of CMake files")
 }
