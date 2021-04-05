34fefc650be63e2cad67dde64926dfd89d59a547
Jules Casteran <jules.casteran@gmail.com>
2021-04-05 16:29:23 -0400

    fix: remove non-used description in main.go and ditch Execute error

--- main.go
+++ main.go
@@ -5,10 +5,9 @@ import (
 )
 
 const version = "0.0.1"
-const description = `A package manager for C++.`
 
 func main() {
 	cmd.RootCmd.Version = version
 	cmd.RootCmd.InitDefaultVersionFlag()
-	cmd.RootCmd.Execute()
+	_ = cmd.RootCmd.Execute()
 }
