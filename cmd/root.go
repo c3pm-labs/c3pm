204cca6dbbefc7f4c293ca1bbca863881ecdf507
Jules Casteran <jules.casteran@gmail.com>
2021-04-05 15:45:27 -0400

    feat: add cobra for cli parsing, change root and add commands to cobra

--- cmd/root.go
+++ cmd/root.go
@@ -1,15 +1,27 @@
 // Package cmd hosts the configuration and handling of the command line interface of C3PM.
 package cmd
 
-import "github.com/alecthomas/kong"
+import (
+	"github.com/alecthomas/kong"
+	"github.com/spf13/cobra"
+)
 
 //CLI is the root configuration of C3PM's command line interface.
 var CLI struct {
 	Version kong.VersionFlag `short:"v" help:"outputs the version number"`
-	Add     AddCmd           `kong:"cmd,help='Add a new dependency'"`
 	Init    InitCmd          `kong:"cmd,help='Init a c3pm project'"`
 	Logout  LogoutCmd        `kong:"cmd,help='Logout from the api'"`
 	Login   LoginCmd         `kong:"cmd,help='Login to the api'"`
 	Build   BuildCmd         `kong:"cmd,help='Build a c3pm project'"`
 	Publish PublishCmd       `kong:"cmd,help='Publish a c3pm project'"`
+}
+
+var RootCmd = &cobra.Command{
+	Use: "ctpm",
+	Short: "c3pm abstracts your build system and eases the management of your dependencies.",
+	Long: "C3PM is a next-generation package manager for C++.\nYou can use C3PM to share and use packages with other developers around the world.",
+}
+
+func init() {
+	RootCmd.AddCommand(addCmd)
 }
--- go.mod
+++ go.mod
@@ -7,7 +7,6 @@ require (
 	github.com/Masterminds/semver/v3 v3.1.1
 	github.com/alecthomas/kong v0.2.12
 	github.com/bmatcuk/doublestar v1.3.4
-	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
 	github.com/mitchellh/go-spdx v0.1.0
@@ -15,6 +14,7 @@ require (
 	github.com/onsi/ginkgo v1.14.2
 	github.com/onsi/gomega v1.10.1
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/spf13/cobra v1.1.3
 	golang.org/x/crypto v0.0.0-20201208171446-5f87f3452ae9 // indirect
 	golang.org/x/sys v0.0.0-20201211090839-8ad439b19e0f // indirect
 	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf // indirect
