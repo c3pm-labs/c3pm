6042a4963900c8227a01cd4acae1f9dcd4e439a1
Jules Casteran <jules.casteran@gmail.com>
2021-04-20 10:29:55 -0400

    fix: main.go conflicts

--- main.go
+++ main.go
@@ -4,11 +4,7 @@ import (
 	"github.com/c3pm-labs/c3pm/cmd"
 )
 
-<<<<<<< HEAD
 var version = "dev"
-=======
-var version = "0.0.1"
->>>>>>> 0d7d584b99cc442c325a8cdb836a11be87c58947
 
 func main() {
 	cmd.RootCmd.Version = version
