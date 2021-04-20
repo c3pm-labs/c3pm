d34894159706ff92380b2d94a986039c9870f725
Codelax <jules.casteran@gmail.com>
2021-04-20 10:16:49 -0400

    fix(adapter): cmake adapter convert path to cmake format (#51)
    Some path in variables used for cmake generation were not converted to cmake format with slashes.

--- adapter/defaultadapter/cmakegen.go
+++ adapter/defaultadapter/cmakegen.go
@@ -101,7 +101,7 @@ func pathListToCmakeVar(paths []string, projectRoot string) string {
 	res := ""
 	for _, path := range paths {
 		res += " "
-		res += filepath.Join(projectRoot, path)
+		res += filepath.ToSlash(filepath.Join(projectRoot, path))
 	}
 	return res
 }
@@ -129,8 +129,8 @@ func varsFromProjectConfig(pc *config.ProjectConfig) (cmakeVars, error) {
 	vars := cmakeVars{
 		ProjectName:      pc.Manifest.Name,
 		ProjectVersion:   pc.Manifest.Version.String(),
-		Sources:          sources,
-		Headers:          headers,
+		Sources:          filepath.ToSlash(sources),
+		Headers:          filepath.ToSlash(headers),
 		IncludeDirs:      pathListToCmakeVar(adapterCfg.IncludeDirs, pc.ProjectRoot),
 		C3PMGlobalDir:    filepath.ToSlash(config.GlobalC3PMDirPath()),
 		Dependencies:     dependencies,
