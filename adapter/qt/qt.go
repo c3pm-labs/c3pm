a8feaab480ee30a4acf2cea84de343f7ba9c63d7
angela-boyadjian <boyadjian.angela@gmail.com>
2021-07-13 15:23:36 +0200

    feat: adapter qt ! Not working

--- adapter/qt/qt.go
+++ adapter/qt/qt.go
@@ -17,9 +17,22 @@ func New() *QtAdapter {
 	return &QtAdapter{}
 }
 
-func execFile(pc *config.ProjectConfig, path string) error {
-	fmt.Println("PATH: " + filepath.Join(path, "configure"))
-	cmd := exec.Command("/bin/sh", filepath.Join(path, "configure"))
+// Working
+func execConfigure(pc *config.ProjectConfig, execPath string, path string) error {
+	cmd := exec.Command("/bin/sh", filepath.Join(execPath, "configure"))
+	cmd.Dir = path
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("%s", out)
+	}
+	return err
+}
+
+func initRepository(pc *config.ProjectConfig, path string) error {
+	fmt.Println("PATH: " + filepath.Join(path, "init-repository"))
+	cmd := exec.Command("perl", filepath.Join(path, "init-repository"))
 	cmd.Dir = path
 	out, err := cmd.Output()
 	if err != nil {
@@ -46,12 +59,76 @@ func executeMakeCli(path string, args ...string) error {
 }
 
 func buildOnLinux(pc *config.ProjectConfig) error {
-	var path = filepath.Join(pc.ProjectRoot, "qtbase")
-	err := execFile(pc, path)
-	if err != nil {
-		return err
+	// var path = filepath.Join(pc.ProjectRoot, "qt5")
+	var pathBuild = filepath.Join(pc.ProjectRoot, "qt6-build")
+	// err := initRepository(pc, path)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// 	return err
+	// }
+	// err := os.Mkdir(pathBuild, 0755)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// 	return err
+	// }
+	// err1 := execConfigure(pc, path, pathBuild)
+	// if err1 != nil {
+	// 	return err1
+	// }
+	err2 := execCmake(pathBuild)
+	if err2 != nil {
+		return err2
 	}
-	return executeMakeCli(path, "-C", path)
+	return nil
+	// return execCmakeInstall(pathBuild)
+}
+
+func execCmakeInstall(path string, args ...string) error {
+	fmt.Println("PATH: " + filepath.Join(path, "init-repository"))
+
+	cmd := exec.Command("cmake", "--install", filepath.Join(path, "."))
+	cmd.Dir = path
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("%s", out)
+	}
+	return err
+	// cmd := exec.Command("cmake", args...)
+	// cmd.Dir = path
+	// cmd.Stdout = os.Stdout
+	// cmd.Stderr = os.Stderr
+	// err := cmd.Start()
+	// if err != nil {
+	// 	return fmt.Errorf("failed to start cmake: %w", err)
+	// }
+	// if err = cmd.Wait(); err != nil {
+	// 	return fmt.Errorf("cmake process failed: %w", err)
+	// }
+	// return nil
+}
+
+func execCmake(path string) error {
+	cmd := exec.Command("cmake", "--build", filepath.Join(path, "."))
+	cmd.Dir = path
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("%s", out)
+	}
+	return err
+	// cmd.Stdout = os.Stdout
+	// cmd.Stderr = os.Stderr
+	// err := cmd.Start()
+	// if err != nil {
+	// 	return fmt.Errorf("failed to start cmake: %w", err)
+	// }
+	// if err = cmd.Wait(); err != nil {
+	// 	return fmt.Errorf("cmake process failed: %w", err)
+	// }
+	// return nil
 }
 
 func (a *QtAdapter) Build(pc *config.ProjectConfig) error {
