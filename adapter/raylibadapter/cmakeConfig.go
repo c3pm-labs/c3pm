ff3aec4b5757ca1693a5460bf0c6311260aea309
Itagiba ALIX <itagiba.alix@gmail.com>
2021-07-16 17:04:47 +0200

    fix: package

--- adapter/raylibadapter/cmakeConfig.go
+++ adapter/raylibadapter/cmakeConfig.go
@@ -1,24 +1,29 @@
-package raylibadapter
+package irrlichtadapter
+
+// set(THREADS_PREFER_PTHREAD_FLAG ON)
+// find_package(Threads REQUIRED)
+// find_package(X11 REQUIRED)
+// find_package(LIBRT rt)
+// find_package(OpenGL REQUIRED)
+// find_package(GLEW REQUIRED)
+
+// include_directories(${OPENGL_INCLUDE_DIR} ${GLEW_INCLUDE_DIRS})
+
+// set(LIBRARIES ${X11_X11_LIB} ${LIBRT} ${CMAKE_DL_LIBS} m Threads::Threads ${OPENGL_LIBRARIES} ${GLEW_LIBRARIES})
 
 var CmakeConfig = `
+
 if ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -lXxf86vm -lXext -lX11 -lGL -lGLU")
-    find_package(OpenGL REQUIRED)
-    find_package(X11 REQUIRED)
-    find_package(GLUT REQUIRED)
-    find_package(ZLIB REQUIRED)
-    set(LIBRARIES ${RAYLIB_LIBRARY} ${OPENGL_LIBRARIES} ${X11_X11_LIB} ${GLUT_LIBRARY} ${ZLIB_LIBRARIES})
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -lraylib -lGL -lm -lpthread -ldl -lrt -lX11")
 endif ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
+
 if (APPLE)
-        find_library(CARBON_LIBRARY CoreVideo)
+        find_library(CARBON_LIBRARY Carbon)
         find_library(COCOA_LIBRARY Cocoa)
         find_library(IOKIT_LIBRARY IOKit)
-        find_library(GLUT_LIBRARY GLUT)
-        find_library(OPENGL_LIBRARY OpenGL)
-        set(OSX_LIBRARIES ${COREVIDEO_LIBRARY} ${COCOA_LIBRARY} ${IOKIT_LIBRARY} ${GLUT_LIBRARY} ${OPENGL_LIBRARY})
-        set(MACOSX_DEPLOYMENT_TARGET ${10.9})
+        set(OSX_LIBRARIES ${CARBON_LIBRARY} ${COCOA_LIBRARY} ${IOKIT_LIBRARY})
 endif (APPLE)
 
-target_include_directories(${C3PM_PROJECT_NAME} PRIVITE src ${RAYLIB_INCLUDE_DIR})
+target_include_directories(${C3PM_PROJECT_NAME} PRIVATE src ${IRRLICHT_INCLUDE_DIR})
 target_link_libraries(${C3PM_PROJECT_NAME} PUBLIC ${OSX_LIBRARIES} ${LIBRARIES})
 `
