8023b52fffc4bfb48de3579ed3168f04c435dcd3
Itagiba ALIX <itagiba.alix@gmail.com>
2021-07-16 17:46:36 +0200

    fix: upadte libs

--- adapter/raylibadapter/cmakeConfig.go
+++ adapter/raylibadapter/cmakeConfig.go
@@ -1,6 +1,9 @@
 package raylibadapter
 
 
+// find_package(LIBRT rt)
+// find_package(GLEW REQUIRED)
+// include_directories(${OPENGL_INCLUDE_DIR} ${GLEW_INCLUDE_DIRS})
 var CmakeConfig = `
 
 if ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
@@ -8,10 +11,7 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -lraylib -lGL -lm -lpthread -ldl -lrt -l
 set(THREADS_PREFER_PTHREAD_FLAG ON)
 find_package(Threads REQUIRED)
 find_package(X11 REQUIRED)
-find_package(LIBRT rt)
 find_package(OpenGL REQUIRED)
-find_package(GLEW REQUIRED)
-include_directories(${OPENGL_INCLUDE_DIR} ${GLEW_INCLUDE_DIRS})
 set(LIBRARIES ${X11_X11_LIB} ${LIBRT} ${CMAKE_DL_LIBS} m Threads::Threads ${OPENGL_LIBRARIES} ${GLEW_LIBRARIES})
 endif ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
 
