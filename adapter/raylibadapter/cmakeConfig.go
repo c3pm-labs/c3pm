062315721b10b8776d8ae3221af3831d33946128
Itagiba ALIX <itagiba.alix@gmail.com>
2021-07-27 16:24:56 +0200

    fix: delete -lraylib

--- adapter/raylibadapter/cmakeConfig.go
+++ adapter/raylibadapter/cmakeConfig.go
@@ -3,7 +3,7 @@ package raylibadapter
 var CmakeConfig = `
 
 if ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -lglfw")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -lGL -lm -lpthread -ldl -lrt -lX11 -lglfw")
         set(THREADS_PREFER_PTHREAD_FLAG ON)
         find_package(Threads REQUIRED)
         find_package(OpenGL REQUIRED)
