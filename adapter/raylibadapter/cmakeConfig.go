df79911043e45b41971ba63806560d1307bdee89
Itagiba ALIX <itagiba.alix@gmail.com>
2021-07-27 15:05:28 +0200

    fix: upadte libs

--- adapter/raylibadapter/cmakeConfig.go
+++ adapter/raylibadapter/cmakeConfig.go
@@ -1,18 +1,15 @@
 package raylibadapter
 
-
-// find_package(LIBRT rt)
-// find_package(GLEW REQUIRED)
-// include_directories(${OPENGL_INCLUDE_DIR} ${GLEW_INCLUDE_DIRS})
 var CmakeConfig = `
 
 if ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -lraylib -lGL -lm -lpthread -ldl -lrt -lX11")
-set(THREADS_PREFER_PTHREAD_FLAG ON)
-find_package(Threads REQUIRED)
-find_package(X11 REQUIRED)
-find_package(OpenGL REQUIRED)
-set(LIBRARIES ${X11_X11_LIB} ${LIBRT} ${CMAKE_DL_LIBS} m Threads::Threads ${OPENGL_LIBRARIES} ${GLEW_LIBRARIES})
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -lraylib -lGL -lm -lpthread -ldl -lrt -lX11")
+        set(THREADS_PREFER_PTHREAD_FLAG ON)
+        find_package(Threads REQUIRED)
+        find_package(X11 REQUIRED)
+        find_package(OpenGL REQUIRED)
+        find_package(LIBRT rt)
+        set(LIBRARIES ${X11_X11_LIB} ${LIBRT} ${CMAKE_DL_LIBS} m Threads::Threads ${OPENGL_LIBRARIES} ${GLEW_LIBRARIES})
 endif ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
 
 if (APPLE)
@@ -25,6 +22,6 @@ if (APPLE)
         set(MACOSX_DEPLOYMENT_TARGET ${10.9})
 endif (APPLE)
 
-target_include_directories(${C3PM_PROJECT_NAME} PRIVITE src ${RAYLIB_INCLUDE_DIR})
 target_link_libraries(${C3PM_PROJECT_NAME} PUBLIC ${OSX_LIBRARIES} ${LIBRARIES})
 `
+// target_include_directories(${C3PM_PROJECT_NAME} PRIVITE src ${RAYLIB_INCLUDE_DIR})
