51e3e05843bf356ce15d8b3f555aa4dd95739a0f
Itagiba ALIX <itagiba.alix@gmail.com>
2021-08-09 17:36:06 +0200

    fix: try add raylib config

--- adapter/raylibadapter/raylibadapter.go
+++ adapter/raylibadapter/raylibadapter.go
@@ -39,7 +39,7 @@ func buildOnMacOS(pc *config.ProjectConfig) error {
 }
 
 func buildOnLinux(pc *config.ProjectConfig) error {
-	return executeCli("make", "-C", pc.ProjectRoot+"/src")
+	return executeCli("mkdir build && cd build && cmake -DBUILD_SHARED_LIBS=ON .. && make", "-C", pc.ProjectRoot+"/src")
 }
 
 func (a *RaylibAdapter) Build(pc *config.ProjectConfig) error {
