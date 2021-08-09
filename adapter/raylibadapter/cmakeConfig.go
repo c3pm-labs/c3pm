c1ff1785b80966133aaf7c36800343edaefccada
Itagiba ALIX <itagiba.alix@gmail.com>
2021-08-09 16:42:39 +0200

    fix: try add raylib config

--- adapter/raylibadapter/cmakeConfig.go
+++ adapter/raylibadapter/cmakeConfig.go
@@ -3,13 +3,14 @@ package raylibadapter
 var CmakeConfig = `
 
 if ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -lglfw3")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -lGL -lm -lpthread -ldl -lrt -lX11")
         set(THREADS_PREFER_PTHREAD_FLAG ON)
         set(USE_EXTERNAL_GLFW ON)
         find_package(Threads REQUIRED)
         find_package(OpenGL REQUIRED)
         find_package (glfw3 REQUIRED)
-        set(LIBRARIES ${CMAKE_DL_LIBS} m Threads::Threads ${OPENGL_LIBRARIES} ${GLEW_LIBRARIES} ${GLFW3_LIBRARY} glfw)
+        find_package (raylib REQUIRED)
+        set(LIBRARIES ${CMAKE_DL_LIBS} m Threads::Threads ${OPENGL_LIBRARIES} ${GLEW_LIBRARIES} ${GLFW3_LIBRARY} glfw raylib)
 endif ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
 
 if (APPLE)
