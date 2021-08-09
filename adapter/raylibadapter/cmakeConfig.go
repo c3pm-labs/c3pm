9eda1aa6fd259175fcefbe04daee5564702a59af
Itagiba ALIX <itagiba.alix@gmail.com>
2021-08-09 16:37:06 +0200

    fix: new include glfw

--- adapter/raylibadapter/cmakeConfig.go
+++ adapter/raylibadapter/cmakeConfig.go
@@ -3,7 +3,7 @@ package raylibadapter
 var CmakeConfig = `
 
 if ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -lGL -lm -lpthread -ldl -lrt -lX11 -lglfw")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -lglfw3")
         set(THREADS_PREFER_PTHREAD_FLAG ON)
         set(USE_EXTERNAL_GLFW ON)
         find_package(Threads REQUIRED)
