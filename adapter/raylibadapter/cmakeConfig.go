57890a7553c99ccff1b10ec9dc699028e9b3f301
Itagiba ALIX <itagiba.alix@gmail.com>
2021-08-16 23:05:35 +0200

    fix: update execCLI

--- adapter/raylibadapter/cmakeConfig.go
+++ adapter/raylibadapter/cmakeConfig.go
@@ -5,6 +5,7 @@ var CmakeConfig = `
 if ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -lGL -lm -lpthread -ldl -lrt -lX11 -lglfw")
         set(THREADS_PREFER_PTHREAD_FLAG ON)
+        set(OpenGL_GL_PREFERENCE LEGACY)
         set(USE_EXTERNAL_GLFW ON)
         find_package(Threads REQUIRED)
         find_package(OpenGL REQUIRED)
