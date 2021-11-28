9c1295540c981567039e61d1a1f3f7a29e8c071f
clement.dubois <cdubois.pro@icloud.com>
2021-11-28 19:48:10 +0100

    feat: add configure adapter

--- adapter/adapter.go
+++ adapter/adapter.go
@@ -3,6 +3,7 @@ package adapter
 import (
 	"errors"
 	"github.com/c3pm-labs/c3pm/adapter/cmake_adapter"
+	"github.com/c3pm-labs/c3pm/adapter/configure_adapter"
 	"github.com/c3pm-labs/c3pm/adapter/defaultadapter"
 	"github.com/c3pm-labs/c3pm/adapter/irrlichtadapter"
 	"github.com/c3pm-labs/c3pm/adapter_interface"
@@ -19,6 +20,8 @@ func (AdapterGetterImp) FromPC(adp *manifest.AdapterConfig) (adapter_interface.A
 		return irrlichtadapter.New(), nil
 	case adp.Name == "cmake" && adp.Version.String() == "0.0.1":
 		return cmake_adapter.New(), nil
+	case adp.Name == "configure" && adp.Version.String() == "0.0.1":
+		return configure_adapter.New(), nil
 	default:
 		return nil, errors.New("only default adapter is supported")
 	}
